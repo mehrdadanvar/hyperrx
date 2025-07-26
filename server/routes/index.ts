6c2f8d999995b58eb64fb861faaa3b2a469b7c81
developer <mehrdad.anvar@gmail.com>
2025-07-25 20:06:06 -0700

    update index

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,7 @@
 let config = useRuntimeConfig();
 console.log(config);
 export default defineEventHandler((event) => {
-  return { data: "Hello World" };
+  return {
+    data: "Welcome to HyperRx API. We are under construction! Currently there is only one route available at hyperrx.hyperneurox.com/api/tm which returns a list of all the prescription drugs in our database.",
+  };
 });
